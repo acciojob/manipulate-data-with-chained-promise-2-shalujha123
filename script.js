81485907bf2add550f2b3f8d05212a32ed9abf05
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-17 21:54:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let output = document.getElementById("output")
+function chainedPromise(arr){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			const evenNumbers = arr.filter((num)=> num%2===0)
+			resolve(evenNumbers)
+		},1000)
+	})
+	.then((evenNumbers)=>{
+		output.innerHTML = evenNumbers.join(", ")
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const doubled = evenNumbers.map((num)=> num*2)
+				resolve(doubled)
+			},2000)
+		})
+	})
+	.then((doubled)=>{
+		output.innerHTML = doubled.join(", ")
+	})
+}
+window.onload = chainedPromise(arr)
+
+
+
+
+
