b59f368cba971e1afb97f122396319e41b3b7c53
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-17 22:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function chainedPromise(arr){
 	return new Promise((resolve)=>{
 		setTimeout(()=>{
 			const evenNumbers = arr.filter((num)=> num%2===0)
+			console.log(evenNumbers)
 			resolve(evenNumbers)
 		},1000)
 	})
@@ -13,12 +14,14 @@ function chainedPromise(arr){
 		return new Promise((resolve)=>{
 			setTimeout(()=>{
 				const doubled = evenNumbers.map((num)=> num*2)
+				console.log(doubled)
 				resolve(doubled)
 			},2000)
 		})
 	})
 	.then((doubled)=>{
 		output.innerHTML = doubled.join(", ")
+		console.log(output.innerHTML)
 	})
 }
 window.onload = chainedPromise(arr)
